8b1c4c6699cd34baae7dfd549b802982522be7b8
Michael <michaelho2016@berkeley.edu>
2014-10-04 16:22:45 -0700

    basic framework for dv_router

--- dv_router.py
+++ dv_router.py
@@ -5,10 +5,20 @@ from sim.basics import *
 Create your distance vector router in this file.
 '''
 class DVRouter (Entity):
-    def __init__(self):
-        # Add your code here!
-        pass
+	routing_table = {}
+	def __init__(self):
+		# Add your code here!
+		create(DVRouter, "dv")
+		routing_table[self] = {self, 0}
 
-    def handle_rx (self, packet, port):
-        # Add your code here!
-        raise NotImplementedError
+	def handle_rx (self, packet, port):
+		# Add your code here!
+		if isinstance(packet, RoutingUpdate):
+			pass
+		elif isinstance(packet, DiscoveryPacket):
+			pass
+		elif isinstance(packet, Packet):
+			pass
+		else
+			pass
+
