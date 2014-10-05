a10ffb6eb49b3fd25a747b75dbc4f517e1b92e4e
Romi Phadte <romiphadte@gmail.com>
2014-10-04 22:19:40 -0700

    added debugging and port_for_packet

--- dv_router.py
+++ dv_router.py
@@ -1,5 +1,6 @@
 from sim.api import *
 from sim.basics import *
+import pdb
 
 '''
 Create your distance vector router in this file.
@@ -15,32 +16,38 @@ class DVRouter (Entity):
 	def handle_rx (self, packet, port):
 		# Add your code here!
 		if isinstance(packet, RoutingUpdate):
-			update_routing_table(packet,port)
+			self.update_routing_table(packet,port)
 			send_update()
 		elif isinstance(packet, DiscoveryPacket):
 			self.routing_table[self][packet.src] = (1, packet.src)
+			state=""
 			if(packet.is_link_up):
+				state="Added "
 				self.ip_to_port[packet.src]=(port,packet.latency)
 			else:
+				state="Removed "
 				self.ip_to_port[packet.src]=(port,None) #didn't use a really high number. 
-				
-			update_routing_table(packet,port)
+
+			print state, packet.src, " to ", self , " table"
+			self.update_routing_table(packet,port)
+
 		elif isinstance(packet, Packet):
-			next_dest = next_hop(packet)
-			self.send(packet, next_dest)
+			port = port_for_packet(packet)
+			self.send(packet, port)
 		else:
+			pdb.set_trace() #TODO:REMOVE. enter debugging if its not any packet
 			pass
 
 
   	def update_routing_table (self, packet, port):
 		pass
 
-	def next_hop (self, packet):
-		result=self.routing_table[self][packet.dst]
-		if result:
-			return result[1] # this is the port
-		return nil 
+	def port_for_packet(self, packet):
+		route=self.routing_table[self][packet.dst]
+		port=self.ip_to_port(route[1])[0]
+		return port
 
 
 	def send_update(self):
+		
 		pass
